dee2f3d45aa779b76e72df85610c49f1a6d9556e
Alex Vasilyeu <f3arnil@gmail.com>
2016-02-29 14:47:15 +0300

    Add static path

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ var config = require('./config.json');
 var path = require('path')
 var app = express();
 
+var uiRootDir = path.join(__dirname + '/' + config.backbone.rootDirectory);
+var staticPath = __dirname + '/' + config.backbone.staticPath;
+
+app.use('/static', express.static(staticPath));
+
+app.listen(config.application.port, function () {
+    console.log('Example app listening on port ' + config.application.port + '!');
+});
+
 app.get('/', function (request, response) {
     response.redirect('/tube-app');
 });
 
 app.get('/tube-app', function (req, res) {
-    var uiRootDir = path.join(__dirname + '/' + config.backbone.rootDirectory);
     res.sendFile(uiRootDir + '/js/tube-app/index.html');
-});
-
-
-app.listen(config.application.port, function () {
-    console.log('Example app listening on port ' + config.application.port + '!');
 });
