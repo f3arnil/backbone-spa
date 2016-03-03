5a56489cecbfb1fe6533e0b954e75b300ec9ce04
Alex Vasilyeu <f3arnil@gmail.com>
2016-03-03 17:01:18 +0300

    Mock data for channels and videos

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ var express = require('express');
 var config = require('./config.json');
 var path = require('path');
 var app = express();
+var channels = require('./mock-data/channels.json');
+var videos = require('./mock-data/videos.json');
 
 var uiRootDir = path.join(__dirname + '/' + config.backbone.rootDirectory);
 var staticPath = __dirname + '/' + config.backbone.staticPath;
 
 app.use('/build', express.static(staticPath));
+
+app.get('/channels', function (req, res) {
+    res.send(channels);
+});
+
+app.get('/videos', function (req, res) {
+    res.send(videos);
+});
 
 app.listen(config.application.port, function () {
     console.log('Example app listening on port ' + config.application.port + '!');
